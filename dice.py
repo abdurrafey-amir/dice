6f80c6f28cdc5e4591f525140e4f7b618aae90e0
Abdurrafey Amir <154964952+abdurrafey-amir@users.noreply.github.com>
2024-08-15 02:09:32 +0500

    finished

--- dice.py
+++ dice.py
@@ -38,3 +38,37 @@ def rolls(x, y, size, radius, color, roll):
     turtle.end_fill()
 
   turtle.forward(size/3)
+
+    if (roll == 1 or roll == 3 or roll == 5):
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+  turtle.forward(size/3)
+
+  if (roll == 6):
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+  turtle.goto(x+(size/6), y-(size/6)-((size/3)*2)-radius)
+
+  if (roll == 4 or roll == 5 or roll == 6):
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+  turtle.forward(size/3)
+
+  if (roll == 2 or roll == 3 or roll == 4 or roll == 5 or roll == 6):
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+def Die(x, y, size, radius, color1, color2):
+  turtle.speed(0)
+  turtle.hideturtle()
+  x -= (size/2)
+  y += (size/2)
+  drawBox(x, y, size, color1)
+  rolls(x, y, size, radius, color2, random.randint(1, 6))
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import dice
 import turtle
 
+def reroll():
+  turtle.clear()
+  turtle.tracer(0, 0)
+  dice.Die(0, 0, 100, 10, "black", "red")
+  dice.Die(0, 150, 100, 10, "black", "red")
+  dice.Die(0, -150, 100, 10, "black", "red")
+  turtle.update()
+  
+
 reroll()
 
 turtle.onkey(reroll, "space")
